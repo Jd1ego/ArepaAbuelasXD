6ab5a9f6039582c60094ecd877bcfbdc4292ec93
Jd1ego <140413545+Jd1ego@users.noreply.github.com>
2025-11-05 17:11:59 -0500

    spring demo

---
+++ src/main/java/com/example/ArepaAbuelas/ArepaAbuelasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArepaAbuelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArepaAbuelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArepaAbuelasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ArepaAbuelas/ArepaAbuelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArepaAbuelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArepaAbuelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
